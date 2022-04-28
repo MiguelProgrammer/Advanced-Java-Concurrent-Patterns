31aa725d9ae375faf09c68bf6cce6dfe930cae09
Miguel Pereira da Silva <miguel.silva@avanade.com>
2022-04-28 15:12:55 -0300

    The Pattern Runnable - 03

--- patterns-concurrent/src/main/java/br/com/estudandoemcasa/PatternsConcurrentApplication.java
+++ patterns-concurrent/src/main/java/br/com/estudandoemcasa/PatternsConcurrentApplication.java
@@ -2,7 +2,11 @@ package br.com.estudandoemcasa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author miguel.silva
+ *
+ */
 @SpringBootApplication
 public class PatternsConcurrentApplication {
 
